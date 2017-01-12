09c093d9b5d9bc4c45ca74a899140e2dd187a30d
bjimenez <bjimenez@sentra.cl>
2017-01-12 06:25:31 -0800

    Se agregan dependencias, gitignore

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http')
 http.createServer(function (request, response){
 	response.writeHead(200,{"Content-Type": "text/plain"})
 	response.end("Hello World\n")
-}).listen(process.env.PORT)
+}).listen(process.env.PORT || 5000)
