0c8970101990a72122e6929b9aab12d355958a31
bjimenez <bjimenez@sentra.cl>
2017-01-12 07:08:35 -0800

    Se acvtu

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var http = require('http')
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
 
 http.createServer(function (request, response){
 	response.writeHead(200,{"Content-Type": "text/plain"})
 	response.end("Hello World\n")
-}).listen(process.env.PORT || 5000)
+})
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
