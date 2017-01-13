060b5be1825d77d01e3321c2c46ddb641a8919ac
bjimenez <bjimenez@sentra.cl>
2017-01-12 17:05:57 -0800

    Actualizaciones mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var cool = require('cool-ascii-faces')
+var express = require('express')
+var app = express()
+var mongoose = require('mongoose')
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,8 +19,26 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+// DB Connection
+mongoose.connect(process.env.MONGODB_URI)
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we are connected!')
+});
+
+var Cat = mongoose.model('Cat', { name: String, createdAt: Date });
+var kitty = new Cat({ name: 'Zildjian', createdAt: new Date() });
+
+kitty.save(function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('meow')
+  }
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-  console.log(process.env.BIENVENIDA);
 });
 
